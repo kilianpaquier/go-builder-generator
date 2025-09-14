6422b48fbf7facfd152b655bae4dc4df5b5c5c91
kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>
2025-09-14 15:22:16 +0200

    ci(mk): use build command instead of manual go build
    Signed-off-by: kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>

--- scripts/mk/custom.mk
+++ scripts/mk/custom.mk
@@ -1,6 +1,5 @@
 .PHONY: testdata
-testdata:
-	@go build -o go-builder-generator ./cmd/go-builder-generator
+testdata: build
 	@find . -name go.mod -execdir go mod tidy \;
 	@find . -name go.mod -execdir go generate ./... \;
 	@echo "It's expected to have the error 'no such tool' from generated files"
